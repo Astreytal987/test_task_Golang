fab9401913999b03b1506ef887ec136d753c11db
Astreytal <sekret.vad@gmail.com>
2024-05-14 07:34:37 +0300

    Roman and arabic calc v1

--- main.go
+++ main.go
@@ -1,9 +1,118 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
+func stringRomanToInt(roman string) (int, error) {
+	romanMap := map[byte]int{'I': 1, 'V': 5, 'X': 10}
+
+	if len(roman) == 0 {
+		return 0, fmt.Errorf("Empty string")
+	}
+
+	for _, char := range roman {
+		if _, ok := romanMap[byte(char)]; !ok {
+			return 0, fmt.Errorf("Не римсккая цифра")
+		}
+	}
+
+	result := romanMap[roman[len(roman)-1]]
+	for i := len(roman) - 2; i >= 0; i-- {
+		if romanMap[roman[i]] < romanMap[roman[i+1]] {
+			result -= romanMap[roman[i]]
+		} else {
+			result += romanMap[roman[i]]
+		}
+	}
+
+	return result, nil
+}
+
+func intToRoman(arabic int) string {
+	arabicMap := map[byte]string{100: "C", 90: "XC", 50: "L", 40: "XL", 10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"}
+
+	roman := ""
+	for arabic > 0 {
+		for value, symbol := range arabicMap {
+			if byte(arabic) >= value {
+				roman += symbol
+				arabic -= int(value)
+				break
+			}
+		}
+	}
+
+	return roman
+}
+
+func calc(num1 int, operator string, num2 int) int {
+	if num1 < 1 || num1 > 10 {
+		panic("число 1 меньше 1 или больше 10")
+	}
+	if num2 < 1 || num2 > 10 {
+		panic("число 2 меньше 1 или больше 10")
+	}
+
+	switch operator {
+	case "+":
+		return num1 + num2
+	case "-":
+		return num1 - num2
+	case "/":
+		return num1 / num2
+	case "*":
+		return num1 * num2
+	default:
+		panic("Это действие нельзя сделать")
+	}
+}
+
 func main() {
-	fmt.Println("this is IT")
+	reader := bufio.NewReader(os.Stdin)
+	equation, _ := reader.ReadString('\n')
+
+	terms := strings.Split(equation, " ")
+	if len(terms) != 3 {
+		panic("Арефметическая операция не удовлетворяет условиям")
+	}
+	operand1Str := terms[0]
+	operator := terms[1]
+	operand2Str := strings.TrimSpace(terms[2])
+	equationRoman := false
+	var operand1int int
+	var operand2int int
+	operand1int, err := strconv.Atoi(operand1Str)
+	if err != nil {
+		operand1int, err = stringRomanToInt(operand1Str)
+		if err != nil {
+			panic("Ведите корректное слагаймое или целое число!")
+		}
+		equationRoman = true
+	}
+	if equationRoman {
+		roman1Num, err1 := stringRomanToInt(operand1Str)
+		roman2Num, err2 := stringRomanToInt(operand2Str)
+		if err1 != nil || err2 != nil {
+			panic("Не римские цифры")
+		}
+		resultInt := calc(roman1Num, operator, roman2Num)
+		if resultInt < 1 {
+			panic("Ответ в риских цифрах должен быть положительным")
+		}
+		resultRoman := intToRoman(resultInt)
+		fmt.Printf("result: %s", resultRoman)
+	}
+	if equationRoman == false {
+		operand2int, err = strconv.Atoi(operand2Str)
+		if err != nil {
+			panic("Вторая переменная не является целым числом")
+		}
+		result := calc(operand1int, operator, operand2int)
+		fmt.Printf("result: %d", result)
+	}
 }
